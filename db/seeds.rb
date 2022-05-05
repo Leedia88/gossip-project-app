f754a6a39e7a0bf556dd21c003ae9f9ff71675bc
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-05-05 23:59:22 +0200

    correction seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,58 +13,50 @@ require 'faker'
 # Gossip.destroy_all
 # Tag.destroy_all
 # TagsGossip.destroy_all
-PrivateMessage.destroy_all
+# PrivateMessage.destroy_all
 
 # "villes
-# 10.times do
-#     city = City.new(name: Faker::Nation.capital_city, zip_code: Faker::Address.postcode)
-#     if city.save
-#       puts "#{city.name}"
-#     end
-# end
+puts "Cities"
+10.times do
+    city = City.new(name: Faker::Nation.capital_city, zip_code: Faker::Address.postcode)
+    if city.save
+      p "#{city.name}"
+    end
+end
 
-# #users
-# 10.times do
-#     firstname = Faker::Name.first_name
-#     lastname = Faker::Name.last_name
-#     email = firstname + "." + lastname + "@thp.com"
-#     User.create!(first_name: firstname, last_name: lastname , email: email, description: Faker::Quote.jack_handey, city_id: Faker::Number.between(from: City.all.first.id, to: City.all.last.id), age: Faker::Number.within(range: 20..40))
-# end
+#users
+puts "Users"
+10.times do
+    firstname = Faker::Name.first_name
+    lastname = Faker::Name.last_name
+    email = firstname + "." + lastname + "@thp.com"
+    user = User.create!(first_name: firstname, last_name: lastname , email: email, description: Faker::Quote.jack_handey, city_id: Faker::Number.between(from: City.all.first.id, to: City.all.last.id), age: Faker::Number.within(range: 20..40))
+    p "#{user.first_name} #{user.first_name}"
+end
 
+#gossips
+5.times do
+    Gossip.create!(title: Faker::Fantasy::Tolkien.poem, content: Faker::Quote.matz, user_id: Faker::Number.between(from: User.all.first.id, to: User.all.last.id) )
+end
+#tags
+10.times do
+    Tag.create!(title: Faker::Emotion.noun)
+end
+#tags_gossips
+    Gossip.all.each do |gossip|
+        nb = rand(1..3)
+        puts "GOSSIP ##{gossip.id} : #{gossip.title} "
+        puts gossip.content
+        p "Tags :"
+        nb.times do
+            tag = TagsGossip.new(tag_id: Faker::Number.between(from: Tag.all.first.id, to: Tag.all.last.id), gossip_id: gossip.id )
+            if tag.save   
+                p Tag.find(tag.tag_id).title + "  "
+            end
+        end
+    end
 
-
-##gossips
-# 20.times do
-#     Gossip.create!(title: Faker::Fantasy::Tolkien.poem, content: Faker::Quote.matz, user_id: Faker::Number.between(from: User.all.first.id, to: User.all.last.id) )
-# end
-
-##tags
-# 10.times do
-#     Tag.create!(title: Faker::Emotion.noun)     
-# end
-
-##tags_gossips
-# 5.times do
-#     Gossip.all.each do |gossip|
-#         nb = rand(1..3)
-#         nb.times do
-#             tag = TagsGos5.times do
-#     Gossip.all.each do |gossip|
-#         nb = rand(1..3)
-#         nb.times do
-#             tag = TagsGossip.new(tag_id: Faker::Number.between(from: Tag.all.first.id, to: Tag.all.last.id), gossip_id: gossip.id )
-#             if tag.save
-#             end
-#         end
-#     end
-# endsip.new(tag_id: Faker::Number.between(from: Tag.all.first.id, to: Tag.all.last.id), gossip_id: gossip.id )
-#             if tag.save
-#             end
-#         end
-#     end
-# end
-
-##Private Messages
+#Private Messages
 5.times do
     pm = PrivateMessage.new(content: Faker::Books::Lovecraft.sentence, sender_id: Faker::Number.between(from: User.all.first.id, to: User.all.last.id), recipient_id: Faker::Number.between(from: User.all.first.id, to: User.all.last.id))
     if pm.save
@@ -75,23 +67,3 @@ PrivateMessage.destroy_all
         puts "message non envoyé!"+"\n"
     end  
 end 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for i in 3..12 do
-#     City.find(i).update(zip_code: Faker::Address.postcode)
-# end
-
-# PrivateMessage.find(1).update(recipient_id: Faker::Number.between(from: User.all.first.id, to: User.all.last.id))
